a357774c2aab03337f6aa92236a76a15bb975de9
brooke <brookedanae88@gmail.com>
2021-03-04 22:38:36 -0500

    First commit

---
+++ Lab18/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var list = new List(10);
+            list.Insert("one");
+            list.Insert("two");
+            list.Insert("three");
+            list.Insert("four");
+
+            list.RemoveAt(11);
+
+            list.PrintReverse();
+
+            list.InsertAt(4, object);
+        }
+    }
+}
